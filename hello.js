c0b409110b18f426c2606746d40f74a1cc7a12be
Muhammad Asgarul Tsani <muhammadasgarultsani@gmail.com>
2025-05-29 07:52:08 +0800

    Add first JavaScript file

---
+++ hello.js
@@ -0,0 +1,7 @@
+console.log("Hello, I'm Astral, and I'm learning JavaScript 🚀");
+
+function greet(name) {
+  return `Hello, ${name}! Welcome to my JavaScript journey.`;
+}
+
+console.log(greet("Visitor"));
