2e13b22fce4fc4dc3a89644b906a073699a02971
Muhammad Asgarul Tsani <muhammadasgarultsani@gmail.com>
2025-05-29 19:28:18 +0800

    add whoareyou

---
+++ whoareyou.js
@@ -0,0 +1,16 @@
+console.log("Hello visitor, who are you?");
+let name = prompt("What your name?");
+let age = prompt("Your age?");
+let country = prompt("Where your country");
+let hobies = prompt("What your hobies?");
+
+alert("Selamat datang, " + name + "!");
+alert(
+  "Your age " +
+    age +
+    " and you live in " +
+    country +
+    " and your hobies is " +
+    hobies +
+    ""
+);
